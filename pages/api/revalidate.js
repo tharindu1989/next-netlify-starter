63174a2e4f1429180fb84d8bbac2e05cc7e2c223
Tharindu Thilanka <tharindu.thilanka@appnovation.com>
2022-06-14 16:13:58 -0700

    update slug in revalidating

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,23 +2,13 @@ export default async function handler(req, res) {
   if (req.body["secret"] !== process.env.NETLIFY_WEBHOOK_SECRET) {
     return res.status(401).json({ message: "Invalid token" });
   }
-  console.log("Update Page : ", req.body["slug"]);
+  console.log("Update Page : ", req.body["slug"]["en-US"]);
   try {
-    await res.unstable_revalidate(`/${req.body["slug"]}`);
+    await res.unstable_revalidate(`/${req.body["slug"]["en-US"]}`);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     return res.status(500).send("Error revalidating");
   }
-}
-function getRawBody(req) {
-  return new Promise((resolve, reject) => {
-    let bodyChunks = [];
-    req.on("end", () => {
-      const rawBody = Buffer.concat(bodyChunks).toString("utf8");
-      resolve(rawBody);
-    });
-    req.on("data", (chunk) => bodyChunks.push(chunk));
-  });
 }
